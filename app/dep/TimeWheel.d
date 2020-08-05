b41d7bfa8046cd3f123950dcb7db45473698d386
tangzhilin <tangzhilinnz@gmail.com>
2020-08-05 13:59:07 +0800

    modify ServerRecyConnThread and ngx_free_connection

--- app/dep/TimeWheel.d
+++
@@ -1,2 +0,0 @@
-/mnt/hgfs/my_nginx/app/link_obj/TimeWheel.o: TimeWheel.cpp /mnt/hgfs/my_nginx/_include/TimeWheel.h \
- /mnt/hgfs/my_nginx/_include/ngx_func.h
--- app/dep/ngx_c_socket_time_abandoned.d
+++
@@ -1,1 +0,0 @@
-/mnt/hgfs/my_nginx/app/link_obj/ngx_c_socket_time_abandoned.o: ngx_c_socket_time_abandoned.cpp
